108a77332570ae709a74eaab13068927ab2a0b93
Igor Dudek <dudekigor@gmail.com>
2020-09-15 16:34:42 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,14 @@
+const { app, BrowserWindow } = require("electron");
+
+let mainWindow;
+
+const createMainWindow = () => {
+  mainWindow = new BrowserWindow({
+    title: "ImageShrink",
+    width: 500,
+    height: 600,
+  });
+  mainWindow.loadURL(`file://${__dirname}/app/index.html`);
+};
+
+app.on("ready", createMainWindow);
