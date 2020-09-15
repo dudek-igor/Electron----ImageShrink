521264e01bf9aa99f0bd155d7e5621e6b8cc2c6c
Igor Dudek <dudekigor@gmail.com>
2020-09-15 17:42:15 +0200

    Add nodemon and solution for checking env and prod

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 const { app, BrowserWindow } = require("electron");
 
+//Set env
+process.env.NODE_ENV = "development";
+
+const isDev = process.env.NODE_ENV !== "production" ? true : false;
+const isMac = process.platform === "darwin" ? true : false;
+
 let mainWindow;
 
 const createMainWindow = () => {
@@ -7,8 +13,24 @@ const createMainWindow = () => {
     title: "ImageShrink",
     width: 500,
     height: 600,
+    icon: `${__dirname}/assets/icons/Icon_256x256.png`,
+    resizable: isDev ? true : false,
+    webPreferences: {
+      nodeIntegration: true,
+    },
   });
-  mainWindow.loadURL(`file://${__dirname}/app/index.html`);
+  mainWindow.loadFile("./app/index.html");
 };
 
+//app
 app.on("ready", createMainWindow);
+app.on("window-all-closed", () => {
+  if (!isMac) {
+    app.quit();
+  }
+});
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createMainWindow();
+  }
+});
